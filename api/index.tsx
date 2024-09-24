1ee5d57be2d12f5a8ad715ac35530c269c18b82a
Dangs <61573413+dtsang2001@users.noreply.github.com>
2024-09-24 22:35:03 +0700

    Update index.tsx

--- api/index.tsx
+++ api/index.tsx
@@ -23,7 +23,7 @@ export const app = new Frog({
     apiUrl: "https://hubs.airstack.xyz",
     fetchOptions: {
       headers: {
-        "x-airstack-hubs": "19d52024f5e694eedbdf857f4a7e84bd8",
+        "x-airstack-hubs": "14cb1f30d1544436194308488fcba51d1",
       }
     }
   }
